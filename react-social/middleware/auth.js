7e23131e117bd1127685050fbbc88172ef44cc97
randy <randyr_1993@yahoo.com>
2022-12-23 02:07:30 -0500

    created logic for authorization

---
+++ react-social/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        let token = req.header("Authorization"); //front end sets the token, then i can grab it in the backend
+
+        if (!token){
+            return res.status(403).send("Access Denied"); //denies access if no token exists
+        }
+
+        //makes sure token starts with bearer string 
+        if (token.startsWith("Bearer ")) {
+            token = token.slice(7, token.length).trimleft();
+        }
+
+        //verifies token then passes in JWT secret string 
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (err){
+        res.status(500).json({ error: err.message })
+    }
+}
